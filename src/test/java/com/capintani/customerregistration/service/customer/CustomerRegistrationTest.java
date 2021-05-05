99097f7c89ce6cf6e5b531832a7488133c7a6c19
José Antonio P. dos Santos <joseantonio.dev@gmail.com>
2021-05-05 13:43:58 -0400

    test: customer search by email implemented

--- src/test/java/com/capintani/customerregistration/service/customer/CustomerRegistrationTest.java
+++ src/test/java/com/capintani/customerregistration/service/customer/CustomerRegistrationTest.java
@@ -2,7 +2,9 @@ package com.capintani.customerregistration.service.customer;
 
 import com.capintani.customerregistration.model.Customer;
 import com.capintani.customerregistration.repository.CustomerRepository;
+import com.capintani.customerregistration.service.AddressService;
 import com.capintani.customerregistration.service.CustomerService;
+import com.capintani.customerregistration.util.AddressWrapperCreator;
 import com.capintani.customerregistration.util.CustomerCreator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,6 +17,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 @ExtendWith(SpringExtension.class)
 @DisplayName("Tests for Customer")
 public class CustomerRegistrationTest {
@@ -23,12 +27,24 @@ public class CustomerRegistrationTest {
     private CustomerService customerService;
     @Mock
     private CustomerRepository customerRepository;
+    @Mock
+    private AddressService addressService;
 
     @BeforeEach
     public void setUp(){
         Customer customer = CustomerCreator.createCustomerToBeFinded();
-        BDDMockito.when(customerRepository.save(ArgumentMatchers.any()))
+        BDDMockito
+                .when(customerRepository.save(ArgumentMatchers.any()))
                 .thenReturn(customer);
+        BDDMockito
+                .when(customerRepository.findByEmail(ArgumentMatchers.anyString()))
+                .thenReturn(Optional.of(customer));
+        BDDMockito
+                .when(customerRepository.findByCpfOrEmail(ArgumentMatchers.anyString(), ArgumentMatchers.anyString()))
+                .thenReturn(Optional.empty());
+        BDDMockito
+                .when(addressService.findByCep(ArgumentMatchers.anyString()))
+                .thenReturn(Optional.of(AddressWrapperCreator.create()));
     }
 
     @Test
@@ -36,6 +52,25 @@ public class CustomerRegistrationTest {
     public void save_Customer_WhenSuccessful(){
         Customer customer = CustomerCreator.createCustomerToBeSaved();
         customer = customerService.save(customer);
+
+        Assertions.assertThat(customer).isNotNull();
+        Assertions.assertThat(customer.getId()).isNotNull();
+        Assertions.assertThat(customer.getName()).isNotNull();
+        Assertions.assertThat(customer.getEmail()).isNotNull();
+        Assertions.assertThat(customer.getCpf()).isNotNull();
+        Assertions.assertThat(customer.getAddress()).isNotNull();
+        Assertions.assertThat(customer.getAddress().getId()).isNotNull();
+        Assertions.assertThat(customer.getAddress().getCep()).isNotNull();
+        Assertions.assertThat(customer.getAddress().getNeighborhood()).isNotNull();
+        Assertions.assertThat(customer.getAddress().getStreetAddress()).isNotNull();
+        Assertions.assertThat(customer.getAddress().getLocality()).isNotNull();
+        Assertions.assertThat(customer.getAddress().getUf()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Tests for find Customer by email")
+    public void find_CustomerByEmail_WhenSuccessful(){
+        Customer customer = customerService.findByEmail("email@gmail.com");
 
         Assertions.assertThat(customer).isNotNull();
         Assertions.assertThat(customer.getId()).isNotNull();
