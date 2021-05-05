740a3558a77bc946620dc5d3f03d3f30167eec5d
José Antonio P. dos Santos <joseantonio.dev@gmail.com>
2021-05-05 14:54:49 -0400

    feat: email search implementede

--- src/main/java/com/capintani/customerregistration/repository/CustomerRepository.java
+++ src/main/java/com/capintani/customerregistration/repository/CustomerRepository.java
@@ -3,7 +3,6 @@ package com.capintani.customerregistration.repository;
 import com.capintani.customerregistration.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 
