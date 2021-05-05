28a8966e91b145d34f234675787113653df8f603
José Antonio P. dos Santos <joseantonio.dev@gmail.com>
2021-05-05 09:47:38 -0400

    feat: entity customer and address add to the project

---
+++ src/main/java/com/capintani/customerregistration/model/Address.java
@@ -0,0 +1,152 @@
+package com.capintani.customerregistration.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table
+public class Address implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "address_sequence", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name="address_sequence", initialValue = 1)
+    private Long id;
+    private String cep;
+    private String streetAddress;
+    private String complement;
+    private String neighborhood;
+    private String locality;
+    private String uf;
+
+    public Address() {
+    }
+
+    public Address(Builder builder) {
+        setId(builder.id);
+        setCep(builder.cep);
+        setStreetAddress(builder.streetAddress);
+        setComplement(builder.complement);
+        setNeighborhood(builder.neighborhood);
+        setLocality(builder.locality);
+        setUf(builder.uf);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep != null ? cep.trim().replaceAll("[^0-9]{8}", "") : cep;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String publicPlace) {
+        this.streetAddress = publicPlace;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String district) {
+        this.neighborhood = district;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(id, address.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public static class Builder {
+        private Long id;
+        private String cep;
+        private String streetAddress;
+        private String complement;
+        private String neighborhood;
+        private String locality;
+        private String uf;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder cep(String cep) {
+            this.cep = cep;
+            return this;
+        }
+
+        public Builder streetAddress(String streetAddress) {
+            this.streetAddress = streetAddress;
+            return this;
+        }
+
+        public Builder complement(String complement) {
+            this.complement = complement;
+            return this;
+        }
+
+        public Builder neighborhood(String neighborhood) {
+            this.neighborhood = neighborhood;
+            return this;
+        }
+
+        public Builder locality(String locality) {
+            this.locality = locality;
+            return this;
+        }
+
+        public Builder uf(String uf) {
+            this.uf = uf;
+            return this;
+        }
+
+        public Address build(){
+            return new Address(this);
+        }
+    }
+}
---
+++ src/main/java/com/capintani/customerregistration/model/Customer.java
@@ -0,0 +1,122 @@
+package com.capintani.customerregistration.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table
+public class Customer implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "customer_sequence", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name="customer_sequence", initialValue = 1)
+    private Long id;
+    private String name;
+    private String cpf;
+    private String email;
+    @JoinColumn
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address address;
+
+    public Customer() {
+    }
+
+    public Customer(Builder builder) {
+        setId(builder.id);
+        setName(builder.name);
+        setCpf(builder.cpf);
+        setEmail(builder.email);
+        setAddress(builder.address);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id.equals(customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public static class Builder {
+        private Long id;
+        private String name;
+        private String cpf;
+        private String email;
+        private Address address;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder cpf(String cpf) {
+            this.cpf = cpf;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder address(Address address) {
+            this.address = address;
+            return this;
+        }
+
+        public Customer build(){
+            return new Customer(this);
+        }
+    }
+}
